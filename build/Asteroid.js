25605f635cbeb579e8f79376eb60e2e997a630e1
Shaun Husain <shaun@intellectual-tech.com>
2023-03-17 21:08:45 -0500

    Made it so asteroids get predrawn and stamped out from graphics objects

--- build/Asteroid.js
+++ build/Asteroid.js
@@ -28,19 +28,19 @@ var Asteroid = (function (_super) {
         var maxWidth = chosenWidth || 40;
         console.log("Asteroid has xVelocity: ".concat(_this.xVelocity, ", yVelocity: ").concat(_this.yVelocity));
         _this.width = Math.random() * maxWidth + 20;
+        _this.height = _this.width;
         _this.sides = _this.p5Instance.random(2) + 5;
+        _this.prerender();
         return _this;
     }
     Asteroid.prototype.draw = function () {
         _super.prototype.draw.call(this);
         this.headingAngle += this.angularVelocity;
         var canvas = this.p5Instance;
-        canvas.strokeWeight(1);
         canvas.push();
         canvas.translate(this.x, this.y);
         canvas.rotate(this.headingAngle);
-        canvas.fill("#555");
-        this.polygon(0, 0, this.width, this.sides);
+        canvas.image(this.cachedGraphics, -this.width / 2, -this.width / 2, this.width * 2, this.width * 2);
         canvas.pop();
     };
     Asteroid.prototype.destroyed = function () {
@@ -55,15 +55,23 @@ var Asteroid = (function (_super) {
             }
         }
     };
-    Asteroid.prototype.polygon = function (x, y, radius, npoints) {
+    Asteroid.prototype.prerender = function () {
+        var canvas = this.p5Instance;
+        this.cachedGraphics = canvas.createGraphics(this.width * 2, this.width * 2);
+        var cg = this.cachedGraphics;
+        cg.strokeWeight(1);
+        cg.fill("#555");
+        this.polygon(this.width / 2, this.width / 2, this.width / 2, this.sides, cg);
+    };
+    Asteroid.prototype.polygon = function (x, y, radius, npoints, cg) {
         var angle = this.p5Instance.TWO_PI / npoints;
-        this.p5Instance.beginShape();
+        cg.beginShape();
         for (var a = 0; a < this.p5Instance.TWO_PI; a += angle) {
             var sx = x + this.p5Instance.cos(a) * radius;
             var sy = y + this.p5Instance.sin(a) * radius;
-            this.p5Instance.vertex(sx, sy);
+            cg.vertex(sx, sy);
         }
-        this.p5Instance.endShape(this.p5Instance.CLOSE);
+        cg.endShape(this.p5Instance.CLOSE);
     };
     return Asteroid;
 }(Rectangle));
--- sketch/Asteroid.ts
+++ sketch/Asteroid.ts
@@ -3,6 +3,9 @@ export class Asteroid extends Rectangle {
   sides: number = 0;
   living = true;
   angularVelocity = .05 + Math.random() * .05
+
+  cachedGraphics: p5.Graphics;
+
   constructor(p5Instance: p5, chosenX?: number, chosenY?: number, chosenWidth?: number) {
     super(p5Instance, 0);
     this.x = chosenX || p5Instance.windowWidth*Math.random()
@@ -14,7 +17,9 @@ export class Asteroid extends Rectangle {
     console.log(`Asteroid has xVelocity: ${this.xVelocity}, yVelocity: ${this.yVelocity}`)
 
     this.width = Math.random()*maxWidth + 20
+    this.height = this.width
     this.sides = this.p5Instance.random(2)+5
+    this.prerender()
   }
   
   draw() {
@@ -22,12 +27,11 @@ export class Asteroid extends Rectangle {
     this.headingAngle+=this.angularVelocity
     
     let canvas: p5 = this.p5Instance;
-    canvas.strokeWeight(1)
     canvas.push()
     canvas.translate(this.x,this.y)
     canvas.rotate(this.headingAngle)
-    canvas.fill("#555")
-    this.polygon(0, 0, this.width, this.sides)
+    // canvas.rect(0,0,this.width,this.height)
+    canvas.image(this.cachedGraphics,-this.width/2,-this.width/2, this.width*2, this.width*2)
     canvas.pop()
   }
 
@@ -44,15 +48,26 @@ export class Asteroid extends Rectangle {
     }
   }
 
-  polygon(x: number, y:number, radius:number, npoints:number) {
+  prerender() {
+    let canvas: p5 = this.p5Instance;
+    this.cachedGraphics = canvas.createGraphics(this.width*2, this.width*2);
+    let cg = this.cachedGraphics;
+    
+    cg.strokeWeight(1)
+    cg.fill("#555")
+    this.polygon(this.width/2, this.width/2, this.width/2, this.sides, cg)
+
+  }
+
+  polygon(x: number, y:number, radius:number, npoints:number, cg:p5.Graphics) {
     let angle = this.p5Instance.TWO_PI / npoints;
-    this.p5Instance.beginShape();
+    cg.beginShape();
     for (let a = 0; a < this.p5Instance.TWO_PI; a += angle) {
       let sx = x + this.p5Instance.cos(a) * radius;
       let sy = y + this.p5Instance.sin(a) * radius;
-      this.p5Instance.vertex(sx, sy);
+      cg.vertex(sx, sy);
     }
-    this.p5Instance.endShape(this.p5Instance.CLOSE);
+    cg.endShape(this.p5Instance.CLOSE);
   }
   
 }
